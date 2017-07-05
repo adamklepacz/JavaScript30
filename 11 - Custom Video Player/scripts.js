21a6db3eb75f080c8cbb58a042c1590b99ee90ff
Adam Klepacz <adamklepacz92@gmail.com>
2017-07-05 22:23:03 +0200

    update

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,40 @@
+/* get elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/* buil functions */
+function togglePlay () {
+	if (video.paused) {
+		video.play();
+	} else {
+		video.pause();
+	}
+}
+
+function updateButton () {
+	var icon;
+	if (this.paused) {
+		icon = '►';
+	} else {
+		icon = '❚ ❚';
+	}
+	toggle.textContent = icon;
+}
+
+function skip () {
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+
+
+/* hook up event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
