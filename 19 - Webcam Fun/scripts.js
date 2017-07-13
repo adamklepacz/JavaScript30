ac7296546ea7cee30fcc4b36a15311abbbe0ddfb
Adam Klepacz <adamklepacz92@gmail.com>
2017-07-13 14:21:47 +0200

    update

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -7,8 +7,25 @@ const snap = document.querySelector('.snap');
 function getVideo () {
 	navigator.mediaDevices.getUserMedia({ video: true, audio: false})
 		.then(localMediaStream => {
-			console.log(localMediaStream);
+			video.src = window.URL.createObjectURL(localMediaStream);
+			video.play();
 		})
+		.catch(err => {
+			console.error(`Smth went wrong!`, err);
+		});
+}
+
+function paintToCanvas () {
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	canvas.width = width;
+	canvas.height = height;
+	
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height);
+	}, 16);
 }
 
 getVideo();
+video.addEventListener('canplay', paintToCanvas);
+
