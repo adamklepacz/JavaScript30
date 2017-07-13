0f1f1fc0dd6a744ff9b31df6ce4ead10ab6636b2
Adam Klepacz <adamklepacz92@gmail.com>
2017-07-13 19:33:42 +0200

    update

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,6 +24,13 @@ function paintToCanvas () {
 	
 	return setInterval(() => {
 		ctx.drawImage(video, 0, 0, width, height);
+		// take the pixels out
+		let pixels = ctx.getImageData(0, 0, width, height);
+		// mess with then
+		pixels = greenScreen(pixels);
+		
+		// pull them back
+		ctx.putImageData(pixels, 0, 0);
 	}, 16);
 }
 
@@ -41,7 +48,50 @@ function takePhoto () {
 	strip.insertBefore(link, strip.firstChild);
 }
 
+function redEffect (pixels) {
+	for(let i = 0; i < pixels.data.length; i+=4) {
+		pixels.data[i + 0] = pixels.data[i + 0] + 150; //red
+		pixels.data[i + 1] = pixels.data[i + 1] - 10;  // green
+		pixels.data[i + 2] = pixels.data[i + 2] * 0.6  // blue
+	}
+	return pixels;
+}
 
+function rgbSplit (pixels) {
+	for(let i = 0; i < pixels.data.length; i+=4) {
+	pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 100] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 450] = pixels.data[i + 2]; // Blue
+	}
+	return pixels;
+}
+
+function greenScreen (pixels) {
+	const levels = {}
+	
+	document.querySelectorAll('.rgb input').forEach((input) => {
+		levels[input.name] = input.value;
+	});
+	
+	for (i = 0; i < pixels.data.length; i+=4) {
+		red = pixels.data[i + 0];
+		green = pixels.data[i + 1];
+		blue = pixels.data[i + 2];
+		aplha = pixels.data[i + 3];
+		
+		if (red >= levels.rmin
+				&& green >= levels.gmin
+				&& blue >= levels.bmin
+				&& red <= levels.rmax
+				&& green <= levels.gmax
+				&& blue <= levels.bmax) {
+			// take it out
+			// set aplha to 0 to make it transparent
+			pixels.data[i + 3] = 0;
+		}
+	}
+	return pixels;
+}
 
 getVideo();
 video.addEventListener('canplay', paintToCanvas);
